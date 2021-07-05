ee2ecc2928471504b40c9beabe7708b3cf4b18d6
Ignacio0104 <ignaci.smirlian@hotmail.com>
2021-07-05 11:16:28 -0300

    Nuevo cambio testo Lunes

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto los escribi primero");
-	alert("Esto lo escribi despues");
+	alert("Esto funciona de maravilla");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,7 +3,7 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	nombre = prompt ("Ingrese su nombre");
+	let nombre = prompt ("Ingrese su nombre");
 	alert ("Hola " + nombre + " cómo estás?")
 }
 
