4306fefb17c2c3d01c38382d453115261608b9c8
Ignacio0104 <ignacio.smirlian@hotmail.com>
2021-07-06 17:41:35 -0300

    Nuevo commits con TPS E/S

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,90 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+
 function Sumar () 
 {
-	
+	let suma;
+    let mensaje;
+    let precio1;
+    let precio2;
+    let precio3;
+
+    precio1= parseFloat (document.getElementById("txtIdPrecioUno").value); 
+
+    precio2= parseFloat (document.getElementById("txtIdPrecioDos").value); 
+
+    precio3= parseFloat (document.getElementById("txtIdPrecioTres").value); 
+
+    suma = precio1 + precio2  + precio3;
+
+    mensaje = "La suma de los 3 productos es $ ";
+
+    alert(mensaje + suma.toFixed(2));
 }
+
 function Promedio () 
 {
+    let suma;
+    let promedio;
+    let mensaje;
+    let precio1;
+    let precio2;
+    let precio3;
+
+    precio1= parseFloat (document.getElementById("txtIdPrecioUno").value); 
+    precio2=parseFloat (document.getElementById("txtIdPrecioDos").value); 
+    precio3=parseFloat (document.getElementById("txtIdPrecioTres").value); 
+
+    suma = precio1 + precio2  + precio3;
+    
+    promedio = suma/3;
+
+    mensaje = "El promedio de los 3 productos es $ ";
+
+    alert(mensaje + promedio.toFixed(2))
 	
 }
 function PrecioFinal () 
 {
-	
+    let suma;
+    let precioFinal;
+    let mensaje;
+    let precio1;
+    let precio2;
+    let precio3;
+    
+
+    precio1= parseFloat (document.getElementById("txtIdPrecioUno").value); 
+    precio2=parseFloat (document.getElementById("txtIdPrecioDos").value); 
+    precio3=parseFloat (document.getElementById("txtIdPrecioTres").value); 
+
+    suma = precio1 + precio2  + precio3;
+    
+    precioFinal = suma * 1.21;
+
+    mensaje = "El precio final de los 3 productos es $ ";
+
+    alert(mensaje + precioFinal.toFixed(2))
 }
+
+/*Comentarios
+
+Hay forma de reducir los decimales?
+
+
+
+let precio1;
+let precio2;
+let precio3;
+
+precio1=document.getElementById("txtIdPrecioUno").value; 
+
+precio2=document.getElementById("txtIdPrecioDos").value;
+
+precio3=document.getElementById("txtIdPrecioTres").value;
+
+No pude ejecutar porque el mensaje era que no se podia leer el valor de .value, que era null.
+
+*/
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,18 +1,82 @@
 /*2.	Para el departamento de Construcción:
-A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
-B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
-C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y 
+se debe alambra con tres hilos de alambre.
+
+B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con 
+tres hilos de alambre.
+
+C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas 
+se necesitan de cada uno para las medidas que nos ingresen.
 */
 
 function Rectangulo () 
 {
+    let perimetro;
+    let mensaje;
+    let mensaje2;
+    let largo;
+    let ancho;
+    let rectanguloAlambre;
+    
 
+    largo = parseInt(document.getElementById("txtIdLargo").value); 
+    ancho = parseInt(document.getElementById("txtIdAncho").value); 
+    
+    perimetro = (largo + ancho) * 2;
+
+    rectanguloAlambre = perimetro * 3;
+
+    mensaje = "La cantidad de alambre que se necesita es de ";
+    mensaje2 = " metros"
+
+    alert(mensaje + rectanguloAlambre + mensaje2);
+    
 }
+
 function Circulo () 
 {
-	
+	let perimetro;
+    let radio;
+    let circuloAlambre;
+    const PI = 3.14;
+
+    let mensaje;
+    let mensaje2;
+    
+    radio = parseInt(document.getElementById("txtIdRadio").value); 
+    
+    perimetro = 2 * PI * radio;
+
+    circuloAlambre = perimetro * 3;
+
+    mensaje = "La cantidad de alambre que se necesita es de ";
+    mensaje2 = " metros"
+
+    alert(mensaje + circuloAlambre  + mensaje2);
 }
+
+
 function Materiales () 
 {
-	
+	let area;
+    let mensaje;
+    let mensaje2;
+    let mensaje3;
+    let largo;
+    let ancho;
+    let cemento;
+    let cal;
+    
+    largo = parseInt(document.getElementById("txtIdLargo").value); 
+    ancho = parseInt(document.getElementById("txtIdAncho").value); 
+    area = largo * ancho;
+
+    cemento = area * 2;
+    cal = area * 3;
+
+    mensaje = "Para este terreno necesitamos ";
+    mensaje2 = " bolsas de cemento y "
+    mensaje3 = " bolsas de cal"
+
+    alert(mensaje + cemento + mensaje2 + cal + mensaje3);
 }
