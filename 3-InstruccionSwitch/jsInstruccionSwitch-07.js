750ed58f01ca630166f61f7cb7c8ca64bed07f9d
Ignacio0104 <ignacio.smirlian@hotmail.com>
2021-07-13 17:23:44 -0300

    Nuevo commit ejercicios Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,28 @@
+/*
+Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste
+
+*/
+
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destino;
+
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch (destino) {
+		case "Bariloche":
+			alert("Se encuentra en el Oeste de Argentina");
+			break;
+		case "Cataratas":
+			alert("Se encuentra en el Norte de Argentina");
+			break;
+		case "Mar del plata":
+			alert("Se encuentra en el Este de Argentina");
+			break;
+		case "Ushuaia":
+			alert("Se encuentra en el sur de Argentina");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,22 @@
+/*
+Al seleccionar un destino informar si hace FRIO o CALOR en ese destino
+*/
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destino;
 
+	destino=document.getElementById("txtIdDestino").value;
+
+	switch (destino){
+		
+		case "Bariloche":
+		case "Ushuaia":
+			alert("Destino de frío");
+			break;
+		case "Mar del plata":
+		case "Cataratas":
+			alert("Destino de calor");
+			break;
+	}
 }//FIN DE LA FUNCIÓN
